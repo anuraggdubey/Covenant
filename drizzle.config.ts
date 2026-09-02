7ac219834635c8f6e239dd4a9f69f325d52a6be9
DubeyJi03 <dubeyanurag3925@gmail.com>
2026-09-02 20:47:44 +0530

    dependecies and permit fixed

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,8 @@
+import dotenv from "dotenv";
 import { defineConfig } from "drizzle-kit";
+
+dotenv.config({ path: ".env.local" });
+dotenv.config();
 
 export default defineConfig({
   schema: "./lib/storage/schema.ts",
@@ -6,3 +10,4 @@ export default defineConfig({
   dialect: "postgresql",
   dbCredentials: { url: process.env.DATABASE_URL ?? "" }
 });
+
